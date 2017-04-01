ea738f23fd90778903c890db589ea8c37404cd6b
Brent <puddletowndesign@gmail.com>
2017-04-01 17:23:42 -0600

    updated atom preferences

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 cd ~/Documents/Dev
-git clone https://github.com/PuddletownDesign/iTerm
-brew cask install iterm
-
 echo "set the theme in preferences in the lower right to load from a folder"
