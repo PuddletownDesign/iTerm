1c6bf53eea92da890538b73397d1b18a3912da15
Brent <puddletowndesign@gmail.com>
2017-09-26 12:45:50 -0500

    updated iterm preferences

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+git remote add upstream https://github.com/PuddletownDesign/Git
+
 cd ~/Dev/Config
 echo "set the theme in preferences in the lower right to load from a folder"
--- update.sh
+++ update.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 cd ~/Dev/Config/iTerm
-git pull origin mac
+git fetch upstream
+git checkout mac
+git merge upstream/mac --no-edit
+
